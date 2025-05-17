377cb0329a03de3977eae633d8e2cec8000a6991
dashcol <v96788271@gmail.com>
2025-05-17 18:19:40 +0530

    new projects and links added

--- src/features/ProjectCard/ProjectCard.jsx
+++ src/features/ProjectCard/ProjectCard.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 
-function ProjectCard({ title, description, imgUrl }) {
+function ProjectCard({ title, description, imgUrl, projectLink }) {
   return (
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="projextimg" />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
+        <a href={projectLink} target="_blank" rel="noopener noreferrer">
+          <img src={imgUrl} alt="projectimg" />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
+        </a>
       </div>
     </Col>
   );
--- src/features/Projects/Projects.jsx
+++ src/features/Projects/Projects.jsx
@@ -6,28 +6,47 @@ import proj1 from "../../assets/img/proj1.png";
 import proj2 from "../../assets/img/proj2.png";
 import proj3 from "../../assets/img/proj3.png";
 import proj4 from "../../assets/img/proj4.png";
+import proj0 from "../../assets/img/proj0.png";
+import projminus1 from "../../assets/img/projminus1.png";
 
 function Projects() {
   const projects = [
     {
+      title: "reachInbox",
+      description: "Mail system Dashboard",
+      imgUrl: projminus1,
+      projectLink: "https://main.d1wlayyoa7jew2.amplifyapp.com/",
+    },
+
+    {
+      title: "Skillswap",
+      description: "Barter system for skills",
+      imgUrl: proj0,
+      projectLink: "https://www.skillswap.dedyn.io/",
+    },
+    {
       title: "GotoO App",
-      description: "Design and Devlopment",
+      description: "Routine Management app",
       imgUrl: proj1,
+      projectLink: "https://main.d1txu1z1p6jc0b.amplifyapp.com/",
     },
     {
       title: "BuyIT",
-      description: "Design and Devlopment",
+      description: "E-commerce App",
       imgUrl: proj2,
+      projectLink: "https://main.d1bac2dviozacd.amplifyapp.com/",
     },
     {
       title: "Crypto Tracker",
-      description: "Design and Devlopment",
+      description: "One place for all crpyto market",
       imgUrl: proj3,
+      projectLink: "https://main.d25dd0l4krq43u.amplifyapp.com/",
     },
     {
       title: "Supe Hunt",
-      description: "Design and Devlopment",
+      description: "Supe Sense",
       imgUrl: proj4,
+      projectLink: "https://vikasrawat17.github.io/supeHunt/",
     },
   ];
   return (
